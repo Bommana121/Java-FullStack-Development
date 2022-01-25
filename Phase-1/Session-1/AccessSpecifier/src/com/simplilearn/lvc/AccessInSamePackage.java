0f1a070cf67f10ef199469e4aa78b02180e1eb2a
unknown <deviprasaddsp143@gmail.com>
2022-01-25 09:50:21 +0530

    Access Specifier

---
+++ Phase-1/Session-1/AccessSpecifier/src/com/simplilearn/lvc/AccessInSamePackage.java
@@ -0,0 +1,15 @@
+package com.simplilearn.lvc;
+
+public class AccessInSamePackage {
+	
+public static void main(String[] args) {
+		
+		AccessSpecifier obj = new AccessSpecifier();
+		obj.display();
+		//obj.test();
+		obj.hello();
+		obj.world();
+	}
+	
+
+}
