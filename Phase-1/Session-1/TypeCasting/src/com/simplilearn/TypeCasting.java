6190ee0111a7aa69d7cb5e0e09a47bc65d52a677
unknown <deviprasaddsp143@gmail.com>
2022-01-27 20:48:40 +0530

    TypeCasting

---
+++ Phase-1/Session-1/TypeCasting/src/com/simplilearn/TypeCasting.java
@@ -0,0 +1,19 @@
+package com.simplilearn.lvc;
+
+public class TypeCasting {
+	public static void main(String[] args) {
+		//Implicit Conversion
+		System.out.println("Implicit type Casting");
+		char a='A';
+		System.out.println("Value of a "+a);
+		int b=a;
+		System.out.println("Value of Int "+b);
+		double x=89.90;
+		int y= (int)x;
+		System.out.println("Value of y "+y);
+		float result = 45.0/7;
+		System.out.println("Value of result "+result);
+	}
+	
+
+}
