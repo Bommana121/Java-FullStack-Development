6d48a2d31ca943bc5c0200ded7ffe880f6b8c769
unknown <deviprasaddsp143@gmail.com>
2022-02-15 09:48:03 +0530

    First and Second WebProject

---
+++ Phase-2/Session-1/SecondWebProject/java/Generic.java
@@ -0,0 +1,28 @@
+package com.simplilearn.com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class Generic extends GenericServlet {
+
+	@Override
+	public void service(ServletRequest req, ServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		
+		String uname = req.getParameter("uname");
+		String pwd = req.getParameter("pwd");
+		if(pwd.equals("admin123")){
+		out.print("Login done successfully");
+		out.print("Welcome "+uname);
+		}
+		else
+			out.print("Invalid Login");
+		
+	}
+
+}
